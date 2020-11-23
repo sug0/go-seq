a577d6c3abcdfd92191ef0d6f553554e56b9e567
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-11-23 05:46:07 +0000

    wip: better cache locality, more heap efficiency

--- seq.go
+++ seq.go
@@ -3,12 +3,14 @@ package sequence
 import (
     "bytes"
     "errors"
-    "encoding/json"
+    //"encoding/json"
 )
 
 type Seq struct {
-    value []byte
-    free  [][]byte
+    free   []byte // 0, x_0, x_1, x_2, ..., x_n-1, N
+    free2  [][]byte
+    value  []byte
+    value2 [256]byte
 }
 
 type seqJSON = struct {
@@ -54,6 +56,9 @@ func Cmp(x, y []byte) int {
 
 // Returns a new sequence starting at 'a'.
 func NewSeq() *Seq {
+    var s Seq
+    s.value2[0] = 'a'
+    s.value = s.value2[:1]
     return &Seq{value: []byte{'a'}}
 }
 
@@ -70,17 +75,25 @@ func NewSeqFrom(value []byte) (*Seq, error) {
 
 // Returns the next ID in the sequence.
 func (s *Seq) Next(nextBuf []byte) []byte {
-    if s.free != nil {
-        lim := len(s.free) - 1
-        pop := s.free[lim]
-        s.free = s.free[:lim]
-        if len(s.free) == 0 {
+    var value []byte
+
+    if len(s.value) < 256 && len(s.free) != 0 {
+        lim := int(s.free[len(s.free) - 1])
+        value = s.free[len(s.free)-lim-1:len(s.free)-1]
+        s.free = s.free[:len(s.free)-lim-2]
+    } else if len(s.value) > 255 && len(s.free2) != 0 {
+        lim := len(s.free2) - 1
+        value = s.free2[lim]
+        s.free2 = s.free2[:lim]
+        if len(s.free2) == 0 {
             s.free = nil // attempt to gc mem
+            s.free2 = nil
         }
-        return pop
+    } else {
+        value = s.value
     }
 
-    nextBuf = append(nextBuf, s.value...)
+    nextBuf = append(nextBuf, value...)
     s.sum()
 
     return nextBuf
@@ -93,61 +106,66 @@ func (s *Seq) Free(value []byte) error {
     if Cmp(value, s.value) >= 0 || s.beenFreed(value) {
         return ErrNotFreeable
     }
-
-    s.free = append(s.free, value)
-    return nil
-}
-
-// Implements json.Marshaler.
-func (s *Seq) MarshalJSON() ([]byte, error) {
-    var buf bytes.Buffer
-
-    buf.Write([]byte(`{"curr":"`))
-    buf.Write(s.value)
-    buf.WriteByte('"')
-
-    if s.free != nil {
-        sz := len(s.free) - 1
-        buf.Write([]byte(`,"free":[`))
-        for i := 0; i < sz; i++ {
-            buf.WriteByte('"')
-            buf.Write(s.free[i])
-            buf.WriteByte('"')
-            buf.WriteByte(',')
-        }
-        buf.WriteByte('"')
-        buf.Write(s.free[sz])
-        buf.WriteByte('"')
-        buf.WriteByte(']')
-    }
-    buf.WriteByte('}')
-
-    return buf.Bytes(), nil
-}
-
-// Implements json.Unmarshaler.
-func (s *Seq) UnmarshalJSON(p []byte) error {
-    var obj seqJSON
-
-    if err := json.Unmarshal(p, &obj); err != nil {
-        return err
-    }
-
-    s.value = []byte(obj.Curr)
-    if obj.Free != nil && len(obj.Free) > 0 {
-        if s.free != nil {
-            s.free = s.free[:0]
-        }
-        sz := len(obj.Free)
-        for i := 0; i < sz; i++ {
-            s.free = append(s.free, []byte(obj.Free[i]))
-        }
+    if len(s.value) < 256 {
+        s.free = append(s.free, 0)
+        s.free = append(s.free, value...)
+        s.free = append(s.free, byte(len(value)))
     } else {
-        s.free = nil
+        s.free2 = append(s.free2, value)
     }
-
     return nil
 }
+
+//     // Implements json.Marshaler.
+//     func (s *Seq) MarshalJSON() ([]byte, error) {
+//         var buf bytes.Buffer
+//     
+//         buf.Write([]byte(`{"curr":"`))
+//         buf.Write(s.value)
+//         buf.WriteByte('"')
+//     
+//         if s.free != nil {
+//             sz := len(s.free) - 1
+//             buf.Write([]byte(`,"free":[`))
+//             for i := 0; i < sz; i++ {
+//                 buf.WriteByte('"')
+//                 buf.Write(s.free[i])
+//                 buf.WriteByte('"')
+//                 buf.WriteByte(',')
+//             }
+//             buf.WriteByte('"')
+//             buf.Write(s.free[sz])
+//             buf.WriteByte('"')
+//             buf.WriteByte(']')
+//         }
+//         buf.WriteByte('}')
+//     
+//         return buf.Bytes(), nil
+//     }
+//     
+//     // Implements json.Unmarshaler.
+//     func (s *Seq) UnmarshalJSON(p []byte) error {
+//         var obj seqJSON
+//     
+//         if err := json.Unmarshal(p, &obj); err != nil {
+//             return err
+//         }
+//     
+//         s.value = []byte(obj.Curr)
+//         if obj.Free != nil && len(obj.Free) > 0 {
+//             if s.free != nil {
+//                 s.free = s.free[:0]
+//             }
+//             sz := len(obj.Free)
+//             for i := 0; i < sz; i++ {
+//                 s.free = append(s.free, []byte(obj.Free[i]))
+//             }
+//         } else {
+//             s.free = nil
+//         }
+//     
+//         return nil
+//     }
 
 func (s *Seq) sum() {
     d := s.value
@@ -169,8 +187,24 @@ func (s *Seq) sum() {
 }
 
 func (s *Seq) beenFreed(value []byte) bool {
-    for i := range s.free {
-        if bytes.Equal(value, s.free[i]) {
+    if len(s.value) < 256 {
+        for i := 0; i < len(s.free); {
+            var j int
+            for j = i+1; j < len(s.free); j++ {
+                if s.free[j] == 0 {
+                    break
+                }
+            }
+            thisValue := s.free[i+1:j-1]
+            if bytes.Equal(value, thisValue) {
+                return true
+            }
+            i += int(s.free[j-1])
+        }
+        return false
+    }
+    for i := 0; i < len(s.free2); i++ {
+        if bytes.Equal(value, s.free2[i]) {
             return true
         }
     }
